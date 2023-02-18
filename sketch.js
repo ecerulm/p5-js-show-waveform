ceac88f1bce63efc33abce19262067b5b6403e10
Ruben Laguna <ruben.laguna@gmail.com>
2023-02-18 12:29:44 +0100

    Add reduced framerate and smoothing

--- sketch.js
+++ sketch.js
@@ -1,31 +1,52 @@
 /* exported setup draw  mousePressed windowResized */
 /* global background noFill stroke resizeCanvas createCanvas */
 /* global windowWidth windowHeight vertex beginShape endShape */
-/* global width map */
+/* global width map lerp */
 let audioContext;
 let analyzerNode;
-let analyzerData;
+let analyzerDataCurrent;
+let analyzerDataTarget;
+let updatesPerSecond = 12;
+let timeMark;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noFill();
+  stroke("white");
+  timeMark = performance.now();
+  setInterval(() => {
+    if (analyzerNode) {
+      analyzerNode.getFloatTimeDomainData(analyzerDataTarget);
+      timeMark = performance.now();
+    }
+  }, 1000 / updatesPerSecond);
 }
 
 function draw() {
   background(0);
-  if (analyzerNode) {
-    noFill();
-    stroke("white");
-    analyzerNode.getFloatTimeDomainData(analyzerData);
 
-    beginShape();
-    for (let i = 0; i < analyzerData.length; i++) {
-      const x = map(i, 0, analyzerData.length, 0, width);
-      const amplitude = analyzerData[i];
-      let y = map(amplitude, -1, 1, windowHeight, 0);
-      vertex(x, y);
-    }
-    endShape();
+  if (!analyzerNode) {
+    return;
   }
+  // interpolate current into target
+  for (let i = 0; i < analyzerDataCurrent.length; i++) {
+    analyzerDataCurrent[i] = lerp(
+      analyzerDataCurrent[i],
+      analyzerDataTarget[i],
+      //0.5,
+      map(performance.now() - timeMark, 0, 1000 / updatesPerSecond, 0, 1, true)
+    );
+  }
+  // console.log(analyzerDataCurrent);
+
+  beginShape();
+  for (let i = 0; i < analyzerDataCurrent.length; i++) {
+    const x = map(i, 0, analyzerDataCurrent.length, 0, width);
+    const amplitude = analyzerDataCurrent[i];
+    let y = map(amplitude, -1, 1, windowHeight, 0);
+    vertex(x, y);
+  }
+  endShape();
 }
 
 function windowResized() {
@@ -46,7 +67,8 @@ function mousePressed() {
         const mic = audioContext.createMediaStreamSource(mediaStream);
         analyzerNode = audioContext.createAnalyser();
         analyzerNode.fftSize = 4096;
-        analyzerData = new Float32Array(analyzerNode.fftSize);
+        analyzerDataCurrent = new Float32Array(analyzerNode.fftSize);
+        analyzerDataTarget = new Float32Array(analyzerNode.fftSize);
         mic.connect(analyzerNode);
       });
   }
